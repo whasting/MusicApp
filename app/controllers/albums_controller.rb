e12734a165a732ae2cf052d77530fa7d984ca1a9
whasting <hastingswade1@gmail.com>
2017-02-16 15:40:28 -0800

    create controller skeles for bands, albums, tracks

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,26 @@
+class AlbumsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def track_params
+  end
+end
